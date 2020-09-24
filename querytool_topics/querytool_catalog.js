f516086f56da8512e36afb47b051c6f0fe6af81d
ptrcrcq <pieter@pietercrucq.nl>
2020-09-24 15:25:11 +0200

    remove maintopic checkboxes when there are no subtopics

--- querytool_topics/querytool_catalog.js
+++ querytool_topics/querytool_catalog.js
@@ -1,5 +1,5 @@
 /**
- * Created by pieter on 16/05/2017.
+ *
  */
 
 jQuery(document).ready(function ($) {
@@ -12,18 +12,27 @@ jQuery(document).ready(function ($) {
     $(all_selectors).each(function( index ) {
 
         $(this).find("input").each(function( index ) {
-            var name = $(this).attr('name');
+            var name = $(this).attr("name");
             var year = name.split("_")[1];
 
-            $(this).change(function(){
-                if( $(this).prop('checked')){
-                    $(this).closest("fieldset.topic").find("input[name*='"+year+"']").prop('checked', true);
-                }else{
-                    $(this).closest("fieldset.topic").find("input[name*='"+year+"']").prop('checked', false);
-                }
-            })
-        });
+            // Hide all selector if no subtopics can be found
+            var subtopics = $(this).closest("fieldset.topic").find("input[name*='"+year+"']");
+            var count = subtopics.length-1; // exclude itself
 
+            if( count == 0){
+                $(this).parent().addClass("unavailabe");
+                $(this).parent().html("x");
+            }else{
+                $(this).change(function(){
+                    if( $(this).prop("checked")){
+                        subtopics.prop("checked", true);
+                    }else{
+                        subtopics.prop("checked", false);
+                    }
+                });
+            }
+
+        });
     });
 
     $(window).scroll(sticky_relocate);
@@ -31,15 +40,15 @@ jQuery(document).ready(function ($) {
 
     function sticky_relocate() {
         var window_top = $(window).scrollTop();
-        var div_top = $('#edit-topic-1').offset().top;
+        var div_top = $("#edit-topic-1").offset().top;
 
         if (window_top > (div_top - 70)) {
-            $('#edit-topic-all').css("position","fixed");
-            $('#edit-topic-all').css("top",0);
-            $('#edit-topic-1').css("margin-top", ($('#edit-topic-all').height()+80) );
+            $("#edit-topic-all").css("position","fixed");
+            $("#edit-topic-all").css("top",0);
+            $("#edit-topic-1").css("margin-top", ($("#edit-topic-all").height()+80) );
         } else {
-            $('#edit-topic-all').css("position","relative");
-            $('#edit-topic-1').css("margin-top",20);
+            $("#edit-topic-all").css("position","relative");
+            $("#edit-topic-1").css("margin-top",20);
         }
     }
 });
